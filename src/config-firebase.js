209cf7a3aa7c24825563172f90e86b4699bf7f94
Tim Arney <tim@line37.com>
2018-01-16 22:02:05 -0500

    testing

--- src/config-firebase.js
+++ src/config-firebase.js
@@ -3,6 +3,9 @@ import firebase from "firebase";
 let db = "";
 let key = "";
 
+
+console.log("env",process.env);
+
 //if (process.env.NODE_ENV === "production") {
   db = process.env.IZ_DB;
   key = process.env.IZ_KEY;
